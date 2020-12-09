04035b3efae129fcb675489c3e08e99303f542b8
NIHILncunia <nihil_ncunia@naver.com>
2020-12-10 02:43:49 +0900

    2020 1210 샘플 코드 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import React, { useState } from 'react';
 const TestApp = () => {
   const [name, setName] = useState('React');
 
+  const onclickButton = () => {
+    if (name === 'React') {
+      setName('Vue');
+    } else {
+      setName('React');
+    }
+  };
+
   return (
-    <p>Hello {name}!!</p>
+    <>
+      <p>Hello {name}!!</p>
+      <button onClick={onclickButton}>변경</button>
+    </>
   );
 };
 
